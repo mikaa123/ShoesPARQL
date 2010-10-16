83bb800f04e4e0bdb652ad77d0db3f94b1cd9549
Michael Sokol <mikaa123@gmail.com>
2010-10-16 19:57:24 -0400

    Adding status label

--- ShoesPARQL.rb
+++ ShoesPARQL.rb
@@ -1,34 +1,42 @@
-require '../SparqlTransmission/SparqlTransmission'
+require '../SparqlTransmission/lib/SparqlTransmission'
 require 'FindPredicats'
 
-Shoes.app :width => 562, :height => 749 do
+Shoes.app :title => "ShoesPARQL", :width => 954, :height => 611 do
 
   background "background.png"
-  stack :width => "100%", :top => 270, :margin => 10 do
-    
-    para link("Open Predicat Explorer") {find_predicats(@url.text)}
-    
-    @url = edit_line "http://dbpedia.org/sparql", :width => "50%"
+
+  para link("Open Predicat Explorer") {find_predicats(@url.text)}, :top => 205, :left => 430
+
+  stack :width => 400, :top => 270, :margin => 30 do
+    @url = edit_line "http://dbpedia.org/sparql", :width => "100%"
     @query = edit_box %[
       SELECT ?subject ?predicat ?object 
       WHERE 
       { 
         ?subject ?predicat ?object
       }
-      ], :width => "100%"
+      ], :width => "100%", :height => 200
+
+    flow do
       button "Execute" do
+        @status.text = "Loading"
         @st = SparqlTransmission.new @url.text
         @st.query = @query.text
         @st.execute_async_query do |res|
 
+          @status.text = "Ready"
           # Shows the results with the full URI
           @results.text = res.collect do |r|
             r.collect { |s| s * ": " } * " | "
           end * "\n"
+        end
       end
-      
+      @status = para "Ready"
     end
+  end
+
+  stack :width => -400, :top => 270, :margin => 30 do
     @results = edit_box :width => "100%", :height => 200
   end
-  
+
 end
